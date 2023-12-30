a54d0f0a3fe1ccfd7950afccc840491d70eb099c
dmodhukuri <root@ip-172-31-24-111.ec2.internal>
2023-12-30 07:26:25 +0000

    added new line

--- usercreation.sh
+++ usercreation.sh
@@ -6,4 +6,4 @@ SPECCHAR=$(echo ${SPEC} | fold -w1 | shuf | head -1)
 PASSWORD=${RANDOM}$(date +%s%N)${SPECCHAR}
 useradd -m ${USER_NAME}
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
-echo "Sucessfully Created user ${USER_NAME} with password as ${PASSWORD}"
+echo "Sucessfully Created user ${USER_NAME} with password as ${PASSWORD} in users list."
