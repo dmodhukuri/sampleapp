a0d4ac2088ea07dae61f2493f24f4ad62ffc480f
dmodhukuri <root@ip-172-31-24-111.ec2.internal>
2024-01-02 10:56:25 +0000

    removed few lines in dev1

--- usercreation.sh
+++ usercreation.sh
@@ -7,6 +7,3 @@ PASSWORD=${RANDOM}$(date +%s%N)${SPECCHAR}
 useradd -m ${USER_NAME}
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 echo "Sucessfully Created user in webserver ${USER_NAME} with password as ${PASSWORD} in users list."
-echo "Sucessfully Created user in webserver ${USER_NAME} with password as ${PASSWORD} in users list in app servers."
-echo "Sucessfully Created user in webserver ${USER_NAME} with password as ${PASSWORD} in users list in db servers."
-echo "Sucessfully Created user in webserver ${USER_NAME} with password as ${PASSWORD} in users list in load balencer."
