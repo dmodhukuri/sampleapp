874a522e9e989153652e1e5dce3f0df4109e1b07
dmodhukuri <root@ip-172-31-24-111.ec2.internal>
2024-01-03 06:48:07 +0000

    add pr

--- usercreation.sh
+++ usercreation.sh
@@ -7,3 +7,4 @@ PASSWORD=${RANDOM}$(date +%s%N)${SPECCHAR}
 useradd -m ${USER_NAME}
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 echo "Sucessfully Created user in webserver ${USER_NAME} with password as ${PASSWORD} in users list."
+fjdskgfn sd.nsdgfsd gndsjgnds 
